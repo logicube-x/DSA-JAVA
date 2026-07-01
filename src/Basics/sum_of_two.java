308cd80891bd2c74dc5a62ff71350362c5204169
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-01 19:36:30 +0530

    sum of two numbers

---
+++ src/Basics/sum_of_two.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class sum_of_two {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+
+        int sum=a+b;
+
+        System.out.println("sum="+sum);
+    }
+
+}
