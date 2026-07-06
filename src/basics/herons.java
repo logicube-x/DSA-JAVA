82e84b67cd6aa6d6671f1d26eaba7d931af7a9e5
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-07 00:00:05 +0530

    herons program

---
+++ src/basics/herons.java
@@ -0,0 +1,19 @@
+package basics;
+
+import java.util.Scanner;
+
+public class herons {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+        int s=(a+b+c)/2;
+
+        double area=Math.sqrt(s*(s-a)*(s-b*(s-c)));
+
+        System.out.println("Area:"+area);
+    }
+
+}
