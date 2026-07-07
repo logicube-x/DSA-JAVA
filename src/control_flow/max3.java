b3cd6b662ce0c576598a3bc95ce2788806c8d3f0
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-07 21:48:03 +0530

    maximum of three numbers

---
+++ src/control_flow/max3.java
@@ -0,0 +1,25 @@
+package control_flow;
+
+import java.util.Scanner;
+
+public class max3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+        if(a>b){
+            if(a>c) System.out.println("a is maximum");
+            else System.out.println("b is maximum");
+        }
+
+        else{
+            if(b>c) System.out.println("b is maximum");
+            else System.out.println("c is maximum");
+
+        }
+    }
+
+}
