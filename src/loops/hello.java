37f57f294c313bfe7cdc60845041f6ccfc82a7c6
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-09 21:44:56 +0530

    beginning of the loops

---
+++ src/loops/hello.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+        int n=sc.nextInt();
+
+    for(int i=1;i<=n;i++){
+        System.out.printf(i+" ");
+    }
+
+    }
+
+}
