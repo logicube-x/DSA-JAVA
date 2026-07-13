8ce60fea6bf66e8dd72f2e5f2dcb6656c6f4783f
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-13 15:25:38 +0530

    a raised to b by for loop

---
+++ src/loops/aPb.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+
+public class aPb {
+    public static void main(String[] args) {
+
+        int a=2;
+        int b=10;
+
+        int result=1;
+        for (int i=1;i<=b;i++){
+            result=result*a;
+        }
+        System.out.println(result);
+    }
+
+}
