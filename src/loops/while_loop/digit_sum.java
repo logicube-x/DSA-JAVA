445ce665be24f580d7526bc329a73d114936d172
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-20 22:22:45 +0530

    printing each digit of given num

--- src/loops/while_loop/digit_sum.java
+++ src/loops/while_loop/digit_sum.java
@@ -2,18 +2,20 @@ package loops.while_loop;
 
 import java.util.Scanner;
 
-public class digit {
+public class digit_sum {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
         int n=sc.nextInt();
         int digit;
+        int sum=0;
+
         while(n!=0){
             digit=n%10;
-            System.out.printf(digit+" ");
+            sum+=digit;
             n=n/10;
         }
-
+        System.out.println(sum);
     }
 
 }
