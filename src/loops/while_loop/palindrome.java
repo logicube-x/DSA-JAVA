9f70deda6304c8a34a9082440433f2d8af4f3ceb
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-27 01:29:19 +0530

    palindrome number checker

---
+++ src/loops/while_loop/palindrome.java
@@ -0,0 +1,20 @@
+package loops.while_loop;
+
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int copy=n;
+        int rev=0;
+        while(n>0){
+            int digit=n%10;
+            rev=(rev*10)+digit;
+            n=n/10;
+        }
+        System.out.println(rev==copy?"Palindorme":"Not Palindrome");
+    }
+
+}
