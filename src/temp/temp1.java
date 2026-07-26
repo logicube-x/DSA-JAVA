c9c57339b81aec56edf377e8f271cd8affdec2a1
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-27 01:16:25 +0530

    reverse a number

--- src/temp/temp1.java
+++ src/temp/temp1.java
@@ -1,8 +1,17 @@
 package temp;
 
+import java.util.Scanner;
+
 public class temp1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int digit;
+        while (n!=0){
+            digit=n%10;
+            n=n/10;
+
+        }
     }
-
 }
