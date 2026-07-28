75f9e1b8a2a20259e28c64d7f80df345bbc425e2
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-28 23:25:56 +0530

    reversing

--- src/temp/temp1.java
+++ src/temp/temp1.java
@@ -1,17 +1,23 @@
 package temp;
 
+import basics.SumOfTwoNumbers;
+
 import java.util.Scanner;
 
 public class temp1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int n=sc.nextInt();
+
         int digit;
+        int revv=0;
         while (n!=0){
             digit=n%10;
-            n=n/10;
+            revv=(revv*10)+digit;
+            n/=10;
 
         }
+        System.out.println(revv);
     }
 }
